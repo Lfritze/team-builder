bb8f94e64a2af6f683d1cfb1972605392c73ec73
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-16 15:17:29 -0400

    Initial commit- removed logo from create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+// import React, { useState } from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      Initial Commit
+    </div>
+  );
+}
+
+export default App;
