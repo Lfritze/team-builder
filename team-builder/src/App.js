2d7f0fc9cc63bd62b36b06f4de1f6fcfb3dc82b9
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-16 10:18:15 -0400

    Imported useState and setting up state

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const [teamMember, setTeamMember] = useState([]);
 
 function App() {
   return (
@@ -25,4 +27,12 @@ function App() {
 
 export default App;
 
+// **************INSTRUCTIONS
+
 // initial commit
+// STEP 1 - Setup your state
+// Import the useState hook and set up a state property for your team members list
+
+// Give the state variable you just declared a default value. You will need to keep track of a list of team members and each team member will have several key/value pairs associated with them.
+
+// Render your list of team members
